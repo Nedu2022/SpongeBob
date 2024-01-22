54fc2b257dae70c9f3c5672e0025d5f5d8313ae0
Nedu2022 <nwabuokeichinedu00@gmail.com>
2024-01-22 01:46:28 +0100

    SpongeBob Project

--- src/SpongeData.js
+++ src/SpongeData.js
@@ -1,52 +1,52 @@
 const SpongeData = [
   {
     title: 'SpongeBob',
-    imageSrc: '/public/SpongeBob_SquarePants_character.svg',
+    imageSrc: '/SpongeBob_SquarePants_character.svg',
     description: 'Card Description goes here...',
   },
   {
     title: 'Patrick Star',
-    imageSrc: '/public/Patrick_Star.png',
+    imageSrc: '/Patrick_Star.png',
     description: 'Card Description goes here...',
   },
   {
     title: 'Squidward',
-    imageSrc: '/public/Squidward.png',
+    imageSrc: '/Squidward.png',
     description: 'Card Description goes here...',
   },
   {
     title: 'Mr. Krabs',
-    imageSrc: '/public/Mr._Krabs.png',
+    imageSrc: '/Mr._Krabs.png',
     description: 'Card Description goes here...',
   },
   {
     title: 'Plankton and Karen',
-    imageSrc: '/public/plankton_and_Karen.png',
+    imageSrc: '/plankton_and_Karen.png',
     description: 'Card Description goes here...',
   },
   {
     title: 'Sandy Cheeks',
-    imageSrc: '/public/Sandy_Cheeks.png',
+    imageSrc: '/Sandy_Cheeks.png',
     description: 'Card Description goes here...',
   },
   {
     title: 'Mrs. Puff',
-    imageSrc: '/public/Mrs._Puff.png',
+    imageSrc: '/Mrs._Puff.png',
     description: 'Card Description goes here...',
   },
   {
     title: 'Pearl the Whale',
-    imageSrc: '/public/Pearl_the_Whale.png',
+    imageSrc: '/Pearl_the_Whale.png',
     description: 'Card Description goes here...',
   },
   {
     title: 'King Neptune',
-    imageSrc: '/public/King_Neptune.png',
+    imageSrc: '/King_Neptune.png',
     description: 'Card Description goes here...',
   },
   {
     title: 'Gary',
-    imageSrc: '/public/Gary.png',
+    imageSrc: '/Gary.png',
     description: 'Card Description goes here...',
   },
 ];
